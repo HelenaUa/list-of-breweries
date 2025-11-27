e98774d5ac7855223d24b2571e61f154470ec108
HelenaUa <delena.1408@gmail.com>
2025-11-27 23:48:49 +0200

    Updated for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import BreweryPage from "./pages/BreweryPage.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/brewery/:id" element={<BreweryPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
