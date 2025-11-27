681565d61ad3500f167a66958f8b7b9e23b2c36f
HelenaUa <delena.1408@gmail.com>
2025-11-28 00:04:22 +0200

    Added basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import BreweryPage from "./pages/BreweryPage.jsx";
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/list-of-breweries">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/brewery/:id" element={<BreweryPage />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
